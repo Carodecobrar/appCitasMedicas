2c700c9d47a34443da806f8c26217e5219cb12d0
Carodecobrar <105404097+Carodecobrar@users.noreply.github.com>
2023-08-21 21:30:26 -0500

    Proyecto inicial

--- app/src/main/java/com/example/citas/MainActivity.kt
+++ app/src/main/java/com/example/citas/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
@@ -14,15 +14,24 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.citas.ui.theme.CitasTheme
 
-data class ActionItem(
-    val name: String,
-    val icon: ImageVector,
-    val action: () -> Unit
+data class Cita(
+    val id: Number,
+    val doctor: String,
+    val paciente: String,
+    val estado: String
 )
+data class Usuario(
+    val id: Number,
+    val nombre: String,
+    val apellido: String,
+    val fechaNacimiento: String,
+    val estado: String
+)
+var index: Number = 0;
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,14 +68,24 @@ fun TopAppBarScreen() {
     var action by remember { mutableStateOf("Ninguna") }
     Column {
         TopAppBar(
-            title = {Text("Citas médicas", color = Color.White)},
+            title = {Text("Citas médicas app", color = Color.White)},
             backgroundColor = Color(0xFF6C7FFD),
             actions = {
                 IconButton(onClick = {taskMenuOpen = true}){
                     Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "Opciones", tint = Color.White)
                     TaskMenu(
                         expanded = taskMenuOpen,
-                        onItemClick = { action = it },
+                        onItemClick = {
+                            if(it == "Ver citas médicas"){
+                                index = 1;
+                            }
+                            if(it == "Ver pacientes"){
+                                index = 2;
+                            }
+                            if(it == "Ver usuarios"){
+                                index = 3;
+                            }
+                        },
                         onDismiss = {
                             taskMenuOpen = false
                         }
@@ -75,10 +94,15 @@ fun TopAppBarScreen() {
             }
         )
         Box(Modifier.fillMaxSize()) {
-            Text(
-                text = "Contenido",
-                modifier = Modifier.align(Alignment.Center)
-            )
+            if (index == 1){
+                TablaCitasMedicas()
+            }
+            if (index == 2){
+                TablaPacientes()
+            }
+            if (index == 3){
+                TablaMedicos()
+            }
         }
     }
 }
@@ -91,11 +115,10 @@ fun TaskMenu(
 ) {
 
     val options = listOf( // (2)
-        "Cambiar nombre",
-        "Enviar por email",
-        "Copiar enlace",
-        "Ocultar subtareas completas",
-        "Eliminar"
+        "Ver citas médicas",
+        "Ver pacientes",
+        "Ver usuarios",
+        "Salir"
     )
 
     DropdownMenu( // (3)
@@ -112,6 +135,103 @@ fun TaskMenu(
             ) {
                 Text(text = option)
             }
+        }
+    }
+}
+
+@Composable
+fun TablaCitasMedicas(){
+    val citas = listOf(
+        Cita(1, "pepito", "pepita", "programada"),
+        Cita(2, "pepito", "pepita", "programada"),
+        Cita(3, "pepito", "pepita", "programada"),
+        Cita(4, "pepito", "pepita", "programada"),
+        Cita(5, "pepito", "pepita", "programada"),
+        Cita(6, "pepito", "pepita", "programada")
+    )
+    Box(Modifier.fillMaxSize()) {
+        ListWithColumnCita(items = citas)
+    }
+}
+@Composable
+fun TablaPacientes(){
+    val pacientes = listOf(
+        Usuario(1, "pepito", "perez", "21/08/2023", "activo"),
+        Usuario(2, "jhon", "linares", "21/08/2023", "activo"),
+        Usuario(3, "andres", "cortes", "21/08/2023", "activo"),
+        Usuario(4, "sergio", "castro", "21/08/2023", "activo"),
+        Usuario(5, "victor", "aguilera", "21/08/2023", "activo"),
+        Usuario(6, "maria", "leon", "21/08/2023", "activo")
+    )
+    Box(Modifier.fillMaxSize()) {
+        ListWithColumnUsuario(items = pacientes)
+    }
+}
+@Composable
+fun TablaMedicos(){
+    val pacientes = listOf(
+        Usuario(1, "juan", "perez", "21/08/2023", "activo"),
+        Usuario(2, "gaspar", "linares", "21/08/2023", "activo"),
+        Usuario(3, "baltazar", "cortes", "21/08/2023", "activo"),
+        Usuario(4, "melchor", "castro", "21/08/2023", "activo"),
+        Usuario(5, "jose", "aguilera", "21/08/2023", "activo"),
+        Usuario(6, "maria", "de los angeles", "21/08/2023", "activo")
+    )
+    Box(Modifier.fillMaxSize()) {
+        ListWithColumnUsuario(items = pacientes)
+    }
+}
+
+@Composable
+fun ListWithColumnCita(items: List<Cita>) { // (1)
+    val scrollState = rememberScrollState()
+
+    Column(modifier = Modifier.verticalScroll(scrollState)) { // (2)
+        items.forEach { item -> // (4)
+            ListItemRowCita(item) // (5)
+        }
+    }
+}
+
+@Composable // (3)
+fun ListItemRowCita(item: Cita, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Text(text = "${item.id} ${item.doctor} ${item.paciente} ${item.estado}", style = MaterialTheme.typography.subtitle1)
+        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 210.dp, top =  0.dp, end = 0.dp, bottom = 0.dp)) {
+            Text(text = "Cambiar estado")
+        }
+    }
+}
+
+@Composable
+fun ListWithColumnUsuario(items: List<Usuario>) { // (1)
+    val scrollState = rememberScrollState()
+
+    Column(modifier = Modifier.verticalScroll(scrollState)) { // (2)
+        items.forEach { item -> // (4)
+            ListItemRowUsuario(item) // (5)
+        }
+    }
+}
+
+@Composable // (3)
+fun ListItemRowUsuario(item: Usuario, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Text(text = "${item.id} ${item.nombre} ${item.apellido} ${item.fechaNacimiento} ${item.estado}", style = MaterialTheme.typography.subtitle1)
+        Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 270.dp, top =  0.dp, end = 0.dp, bottom = 0.dp)) {
+            Text(text = "Actualizar")
         }
     }
 }
